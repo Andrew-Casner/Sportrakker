cc7110efb8815e0f840b4ec4e91b5aa89858a604
andrew-casner <a.casner@comcast.net>
2016-04-12 17:19:17 -0600

    Added a get all players route in ./app/routes/player.js

--- app/routes/player.js
+++ app/routes/player.js
@@ -21,5 +21,13 @@ router.route('/player').post(function(req, res){
 				});
 });
 
+router.route('/player').get(function(req, res){
+				Player.find(function(err, player){
+								if(err){
+												return res.send(err);
+								}
+								res.json(player);
+				});
+});
 // Export the router to be used in server.js
 module.exports = router;
