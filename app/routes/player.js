2384ffaef4622111936a06d1157b439e3d4ab182
andrew-casner <a.casner@comcast.net>
2016-04-12 12:53:05 -0600

    Updated API routes for player.js

--- app/routes/player.js
+++ app/routes/player.js
@@ -1,5 +1,14 @@
 // app/routes/player.js
 
+
+var express = require('express');
+var app = express();
+var router = express.Router();
+
 //===============API ROUTES==================
 
+app.get('/', function(req, res){
+				res.send('Hello World');
+});
 
+app.listen(8080);
