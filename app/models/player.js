a11afd73e61369b40c2985df44c45cbc37616b63
andrew-casner <a.casner@comcast.net>
2016-04-13 17:42:29 -0600

    Added basic info for all Players

--- app/models/player.js
+++ app/models/player.js
@@ -4,7 +4,21 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var PlayerSchema = new Schema({
+				// Basic Info
 				name: String
+				isMale: Boolean
+				height: Number 
+				weight: Number
+				birthday: Date
+ 				hometown: String
+				// Athletic Info
+				bench: [Number]
+				squat: [Number]
+				power: [Number]
+				dash: [Number]
+				shuffle: [Number]
+				vertical: [Number]
+				// Other Info
 });
 
 module.exports = mongoose.model('Player', PlayerSchema);
