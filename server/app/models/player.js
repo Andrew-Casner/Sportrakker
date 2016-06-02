bb8a5c027f9855ca52c174e19beff2e394d1d8e7
andrew-casner <a.casner@comcast.net>
2016-06-01 20:25:31 -0600

    Moved all of the server elements to their own file titled 'server'

---
+++ server/app/models/player.js
@@ -0,0 +1,38 @@
+// app/models/player.js
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var PlayerSchema = new Schema({
+				// Basic Info
+				firstName: String,
+				lastName: String,
+				isMale: Boolean,
+				height: Number, 
+				weight: Number,
+				birthday: Date,
+ 				hometown: String,
+				// Athletic Info
+				// - stored as arrays so it can be graphed over time
+				// - need to add in time component
+				// - talk to Dawson about json objects
+				bench: [Number],
+				benchTimeStamp: [Date],
+				squat: [Number],
+				squatTimeStamp: [Date],
+				power: [Number],
+				powerTimeStamp: [Date],
+				dash: [Number],
+				dashTimeStamp: [Date],
+				shuffle: [Number],
+				shuffleTimeStamp: [Date],
+				vertical: [Number],
+				verticalTimeStamp: [Date],
+				// Other Info
+				// Sports played
+				sports: [String],
+				// Teams on
+				teams: [String]
+});
+
+module.exports = mongoose.model('Player', PlayerSchema);
