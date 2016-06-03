1200185130be52bdb98bf5b1780d18ffd7d6f0ea
andrew-casner <a.casner@comcast.net>
2016-06-02 23:21:32 -0600

    Creating and getting an organization work

--- server/app/routes/organization.js
+++ server/app/routes/organization.js
@@ -8,13 +8,23 @@ var Organization = require('../models/organization');
 //===============API ROUTES==================
 
 // Create an Organization
-router.route('/organization').post(function(req, res){P
+router.route('/organization').post(function(req, res){
 				var organization = new Organization(req.body);
 				organization.save(function(err){
 								if(err){
 												return res.sender(err);
 								}
 								res.send({message: 'Organization Created'});
+				});
+});
+
+// Get all Organizations
+router.route('/organization').get(function(req, res){
+				Organization.find(function(err, organization){
+								if(err){
+												return res.send(err);
+								}
+								res.json(organization);
 				});
 });
 
