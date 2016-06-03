27e19e7c89c64c4608e00b7b021ada3926b99a38
andrew-casner <a.casner@comcast.net>
2016-06-02 23:01:52 -0600

    Added Organization Schema, no data filled out, but the basic infastructure is in place

---
+++ server/app/models/organization.js
@@ -0,0 +1,11 @@
+// app/models/organization.js
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var OrganizationSchema = new Schema({
+				// Basic Info
+				
+});
+
+module.exports = mongoose.model('Organization', OrganizationSchema);
