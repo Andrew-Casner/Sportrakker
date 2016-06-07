403d07a163deb122d66e9f0450dc3280da65617b
andrew-casner <a.casner@comcast.net>
2016-06-06 21:58:16 -0600

    Finished all detains of team, need to discus more on more specific fields

--- server/app/models/team.js
+++ server/app/models/team.js
@@ -5,7 +5,14 @@ var Schema = mongoose.Schema;
 
 var TeamSchema = new Schema({
 				//Basic Info
-				teamName: String
+				name: String,
+				sex: Number,  // 0 = female, 1 = male
+				organization: String,  // id of affiliated organization
+				staff: [String],  // id of all staff
+				players: [String],  // id of all the players on the team
+				// Results from meets, needs to be discused
+				results: [String]  // id of all results
+
 });
 
 module.exports = mongoose.model('Team', TeamSchema);
