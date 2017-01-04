e29645b173c9d942adf03c18edeb46ae3250d99d
Andrew Casner <a.casner@comcast.net>
2017-01-03 23:07:20 -0700

    Fixed landing page for the app for the live site

--- sportrakker/src/app/app.component.ts
+++ sportrakker/src/app/app.component.ts
@@ -4,10 +4,11 @@ import { NewMeetComponent} from './new-meet/new-meet.component';
 
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Welcome to Sportrakker Dad!!!!!!!';
+	title = 'Welcome to Sportrakker!';
+	description = 'We are currently under development, check back later for more content';
 }
