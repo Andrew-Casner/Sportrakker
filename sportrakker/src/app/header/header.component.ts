437ba60dd83740a86ffc7748ed832efc8aa1af05
Andrew Casner <a.casner@comcast.net>
2017-01-07 21:50:55 -0700

    Added validatio nto login page

--- sportrakker/src/app/header/header.component.ts
+++ sportrakker/src/app/header/header.component.ts
@@ -1,14 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+	selector: 'app-header',
+	templateUrl: './header.component.html',
+	styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+	login : FormGroup;
+	constructor(fb: FormBuilder) {
+		this.login = fb.group({
+			'username' : [null, Validators.required],
+			'password' : [null, Validators.required]
+		})
+	}
+	submitForm(value: any):void{
+		console.log(value);
+	}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 }
